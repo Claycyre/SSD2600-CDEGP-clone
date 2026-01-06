7710b29b924a834561070bd84e4e5ea45e6e0b9e
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2026-01-06 11:54:47 -0800

    chore: add dev flake;

---
+++ flake.nix
@@ -0,0 +1,24 @@
+# flake.nix - Nix Flake configuration file
+# This file defines a reproducible development environment using Nix flakes
+# Documentation: https://nixos.wiki/wiki/Flakes
+{
+  description = "SSDP 2600 Project environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    # This function creates outputs for each system (x86_64-linux, aarch64-darwin, etc.)
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { system = "x86_64-linux"; };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "SSD2600";
+          nativeBuildInputs = with pkgs; [ zsh ];
+
+          buildInputs = with pkgs; [ git dotnet-sdk ];
+        };
+      });
+}
