89bb13f67db479eba553ab7bda29b6e02c2e422a
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2026-01-06 12:03:20 -0800

    chore: bootstrap initial application;

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SSD2600_CDEGP.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
