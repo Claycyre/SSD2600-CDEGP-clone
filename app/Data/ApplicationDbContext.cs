d150e4c16aa9bc0850fa255a54e8ccad6ae11d97
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2026-02-02 11:04:27 -0800

    style: run dos2unix project-wide;

--- app/Data/ApplicationDbContext.cs
+++ app/Data/ApplicationDbContext.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace SSD2600_CDEGP.Data;
