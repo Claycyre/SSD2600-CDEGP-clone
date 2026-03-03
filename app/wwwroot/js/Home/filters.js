448f8485355bf11279bec1fd3247c464ef2d32a2
Geoff Warner <231114443+geoff-warner@users.noreply.github.com>
2026-03-03 10:58:30 -0800

    Move filters.js to remote from in-file

---
+++ app/wwwroot/js/Home/filters.js
@@ -0,0 +1,11 @@
+﻿namespace SSD2600_CDEGP.wwwroot.js.Home;
+function clearFilters() {
+  // Uncheck all checkboxes
+  document.querySelectorAll('input[name="Phases"]').forEach(cb => cb.checked = false);
+
+  // Reset sort to default (no value)
+  document.querySelector('input[name="SortBy"][value=""]').checked = true;
+
+  // Submit form
+  document.getElementById('filterForm').submit();
+    }
