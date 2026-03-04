155f03662c33ab1c1a7345ecdabe924a2f9cc16c
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2026-03-03 18:16:28 -0800

    chore(periodic-filters): move filters.js to src dir;

--- app/src/filters.ts
+++ app/src/filters.ts
@@ -1,3 +1,23 @@
+function clearFilters() {
+  // Uncheck all checkboxes
+  document
+    .querySelectorAll('input[name="Phases"]')
+    //@ts-ignore
+    .forEach((cb) => (cb.checked = false));
+  document
+    .querySelectorAll('input[name="types"]')
+    //@ts-ignore
+    .forEach((cb) => (cb.checked = false));
+
+  // Reset sort to default (no value)
+  //@ts-ignore
+  document.querySelector('input[name="SortBy"][value=""]').checked = true;
+
+  // Submit form
+  //@ts-ignore
+  document.getElementById("filterForm").submit();
+}
+
 function filtersOnSubmit(_: SubmitEvent) {
   window.location.hash = "#periodic-table";
 }
