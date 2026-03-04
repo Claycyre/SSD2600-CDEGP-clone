82cc3a3c66b536b8afb07fd0cfda74c8c99cd19d
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2026-03-03 21:58:02 -0800

    feat(landing-page): add scroll to periodic table CTA;

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,1 +1,8 @@
 console.log("Loaded JS");
+
+function scrollToElement(id: string, opts?: ScrollIntoViewOptions) {
+  const element = document.getElementById(id);
+  if (!element) return;
+
+  element.scrollIntoView(opts);
+}
